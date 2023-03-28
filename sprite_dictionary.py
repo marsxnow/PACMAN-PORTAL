bf2b19851df268a795733b8ea9883d3f1a79e409
raxelortiz7 <raxelortiz7@csu.fullerton.edu>
2023-03-28 15:46:53 -0400

    pusing new sprite sheet and sprite dictionary file

---
+++ sprite_dictionary.py
@@ -0,0 +1,12 @@
+from spritesheet import Spritesheet
+import pygame as pg
+
+class SpriteDict:
+    def __init__(self):
+        self.sprite_sheet = Spritesheet('image/spritesheet.png')
+        self.pacman
+        self.blinky
+        self.clyde
+        self.inkey
+        self.portal
+        self.pac_life
